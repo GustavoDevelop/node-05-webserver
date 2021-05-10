b4973c1b36934317c80771eb36a29b46e0a94489
GustavoDevelop <gustavitoalanes07@gmail.com>
2021-05-10 11:48:27 -0300

    Subi mi server a postman y puse para descargar

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const http = require('http');
 
 
 http.createServer((req, res) =>{
+
     console.log(req);
 
-    res.write('русский язык, romanización russki yazyk, pronunciación [ˈruskʲɪj jɪˈzɨk');
+    
+    //res.writeHead(200, {'Content-type': 'application/json'});
+    /*const user = {
+        id: 123,
+        name: 'Fernando'
+    }
+    res.write(JSON.stringify(user));
+    */
+    res.setHeader('Content-Disposition', 'attachment; filename=usuarios.csv');
+    res.writeHead(200, {'Content-type': 'application/csv'});
+    res.write('id,name\n123,Fernando\n456,Marta');
+
     res.end();
 }).listen(3000);
 
